15125c3ac3baa3d7695ce30ff5ba0fdbbb56ac97
AndreyIon <159322759+AndreyIon@users.noreply.github.com>
2024-02-12 01:37:57 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,28 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-System.out.println("Hello, world");
-}
 
+	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("Andrey");
+
+		double l = 5;
+		System.out.println("Площадь квадрата с стороной " + l + " = " + area(l));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello," + somebody + "!");
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b) );
+	}
+
+	public static double area (double len){
+    return len * len;
+	}
+	public static double area (double a, double b) {
+		return a * b;
+	}
 }
