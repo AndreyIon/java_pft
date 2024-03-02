c59bdac1671e1940acc778c86cc76d3ee51af3a9
AndreyIon <159322759+AndreyIon@users.noreply.github.com>
2024-03-02 23:44:45 +0500

    Заменяем наследование и выделяем специализированные классы-помощники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationHelper {
+  private WebDriver driver;
+
+  public NavigationHelper(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public void gotoGrouoPage() { driver.findElement(By.linkText("groups")).click(); }
+}
