081e302e978f3b1f57a37d696212d8b109761847
AndreyIon <159322759+AndreyIon@users.noreply.github.com>
2024-04-04 10:35:56 +0500

    Как правильно запускать браузер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -2,21 +2,23 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import static org.testng.Assert.fail;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(String.valueOf(Browser.FIREFOX));
+  protected static final ApplicationManager app = new ApplicationManager(String.valueOf(Browser.FIREFOX));
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
   }
